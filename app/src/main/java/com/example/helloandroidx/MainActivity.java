e5ce8e9c336be03359644e1bb6da26ab34ec9084
Wilson Guiovanny Muñoz Daza <wilson176@unicauca.edu.co>
2019-05-05 16:34:01 -0500

    Withou time and date update

--- app/src/main/java/com/example/helloandroidx/MainActivity.java
+++ app/src/main/java/com/example/helloandroidx/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        updateTime();
+        
+        //updateTime();
     }
 }
